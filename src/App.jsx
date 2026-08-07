89ab1571a5d4e805469e9ce9da301d6bd5abea1e
ameyy02 <ameypawar209@gmail.com>
2026-08-07 22:48:08 +0530

    emp1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Login from './components/auth/Login'
+import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 
 const App = () => {
   return (
     <>
-    <Login/>
+    <EmployeeDashboard/>
     </>
   )
 }
---
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../others/Header'
+
+const EmployeeDashboard = () => {
+  return (
+    <div className='p-7'>
+        <Header/>
+    </div>
+  )
+}
+
+export default EmployeeDashboard
