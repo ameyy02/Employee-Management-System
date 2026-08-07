504c28c7e784d56110d761cac3c99ee7ccda9353
Amey Pawar <ameypawar209@gmail.com>
2026-08-08 00:36:49 +0530

    Employee dashboard ui making

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../others/Header'
+import Tasklistnum from '../others/Tasklistnum'
+import Tasklist from '../Tasklist/Tasklist'
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-7'>
         <Header/>
+        <Tasklistnum/>
+        <Tasklist/>
     </div>
   )
 }
---
+++ src/components/others/Tasklistnum.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Tasklistnum = () => {
+  return (
+    <div className='mt-7 flex gap-7 w-full'>
+        <div className='bg-[#111827] h-70 w-full p-3 border border-[#1F2937] rounded-2xl shadow-xl shadow-black/20'>
+            <h1 className='text-4xl font-semibold'>0</h1>
+            <h1 className='mt-2'>New Task</h1>
+        </div>
+        <div className='bg-[#111827] h-70 w-full p-3 border border-[#1F2937] rounded-2xl shadow-xl shadow-black/20'>
+            <h1 className='text-4xl font-semibold'>0</h1>
+            <h1 className='mt-2'>New Task</h1>
+        </div>
+        <div className='bg-[#111827] h-70 w-full p-3 border border-[#1F2937] rounded-2xl shadow-xl shadow-black/20'>
+            <h1 className='text-4xl font-semibold'>0</h1>
+            <h1 className='mt-2'>New Task</h1>
+        </div> 
+        <div className='bg-[#111827] h-70 w-full p-3 border border-[#1F2937] rounded-2xl shadow-xl shadow-black/20'>
+            <h1 className='text-4xl font-semibold'>0</h1>
+            <h1 className='mt-2'>New Task</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Tasklistnum
